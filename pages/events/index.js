c0c1a0e37e999ff6d414e7282beeda750cc76fb8
Muhamed Mehmedovic <54117777+Muhamed112@users.noreply.github.com>
2021-09-14 12:56:21 +0200

    Initial commit

---
+++ pages/events/index.js
@@ -0,0 +1,44 @@
+import { getAllEvents } from "../../helpers/api-util";
+import Head from "next/head";
+import EventList from "../../components/events/event-list";
+import EventsSearch from "../../components/events/events-search";
+import { Fragment } from "react";
+import { useRouter } from "next/router";
+
+function AllEventsPage(props) {
+  const { events } = props;
+  const router = useRouter();
+
+  function findEventsHandler(year, month) {
+    const fullPath = `/events/${year}/${month}`;
+
+    router.push(fullPath);
+  }
+
+  return (
+    <Fragment>
+      <Head>
+        <title>All Events</title>
+        <meta
+          name="description"
+          content="Find a lot of great events that allow you to evolve"
+        />
+      </Head>
+      <EventsSearch onSearch={findEventsHandler} />
+      <EventList items={events} />
+    </Fragment>
+  );
+}
+
+export default AllEventsPage;
+
+export async function getStaticProps() {
+  const events = await getAllEvents();
+
+  return {
+    props: {
+      events: events,
+    },
+    revalidate: 60,
+  };
+}
